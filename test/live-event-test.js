dd067c8e99edb4075402bb2d766a438d6384f28a
Francisco Souza <francisco.souza@nytimes.com>
2017-02-03 18:13:11 -0500

    live-event: add eventStatus

--- test/live-event-test.js
+++ test/live-event-test.js
@@ -22,4 +22,14 @@ describe('LiveEvent', () => {
     assert.equal(result, retval);
     assert(client.sendRequest.calledWith('GET', '/api/live_events/199/inputs'));
   });
+
+  it('eventStatus should retrieve the status of an event', () => {
+    const retval = {key: 'value'};
+    const client = {sendRequest: sinon.stub().returns(retval)};
+    const le = new LiveEvent(client);
+    const result = le.eventStatus(199);
+
+    assert.equal(result, retval);
+    assert(client.sendRequest.calledWith('GET', '/api/live_events/199/status'));
+  })
 });
