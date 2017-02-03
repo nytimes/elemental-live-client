87367893e5a75daa6a15e35d934a70f0160ea4c2
Francisco Souza <francisco.souza@nytimes.com>
2017-02-03 13:26:31 -0500

    elemental-client: store authentication credentials

--- test/elemental-client-test.js
+++ test/elemental-client-test.js
@@ -1,7 +1,13 @@
+import {assert} from 'chai';
+
 import ElementalClient from '../lib/elemental-client';
 
 describe('ElementalClient', () => {
   it('should receive credentials on the constructor', () => {
     const client = new ElementalClient('http://my-elemental-server', 'user', 'key');
+
+    assert.equal(client.serverUrl, 'http://my-elemental-server');
+    assert.equal(client.user, 'user');
+    assert.equal(client.key, 'key');
   });
 });
