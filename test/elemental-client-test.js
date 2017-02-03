6343c34681103db442a6ee2db89a8ac85c21d44a
Francisco Souza <francisco.souza@nytimes.com>
2017-02-03 14:50:05 -0500

    elemental-client: implement sendRequest

--- test/elemental-client-test.js
+++ test/elemental-client-test.js
@@ -1,13 +1,39 @@
-import {assert} from 'chai';
-
 import ElementalClient from '../lib/elemental-client';
+import {assert} from 'chai';
+import sinon from 'sinon';
 
 describe('ElementalClient', () => {
-  it('should receive credentials on the constructor', () => {
-    const client = new ElementalClient('http://my-elemental-server', 'user', 'key');
+  it('sendRequest without data', () => {
+    const client = new ElementalClient('http://my-elemental-server');
+    const callback = (data) => data;
 
-    assert.equal(client.serverUrl, 'http://my-elemental-server');
-    assert.equal(client.user, 'user');
-    assert.equal(client.key, 'key');
+    client.req = sinon.stub();
+    client.sendRequest('GET', '/api/live_events', {page: '3', 'per_page': 30}, null, callback);
+
+    assert(client.req.calledWith({
+      method: 'GET',
+      url: '/api/live_events',
+      qs: {'page': '3', 'per_page': 30},
+      headers: {},
+      json: false,
+      body: null,
+    }, callback))
+  });
+
+  it('sendRequest with data', () => {
+    const client = new ElementalClient('http://my-elemental-server');
+    const callback = (data) => data;
+
+    client.req = sinon.stub();
+    client.sendRequest('POST', '/api/live_events', null, {name: 'My Live Event'}, callback);
+
+    assert(client.req.calledWith({
+      method: 'POST',
+      url: '/api/live_events',
+      qs: null,
+      headers: {'Content-Type': 'application/json'},
+      json: true,
+      body: {name: 'My Live Event'},
+    }, callback))
   });
 });
