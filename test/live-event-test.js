dc6e26e07a028c69f84abb3798e0cf04d35087db
Francisco Souza <francisco.souza@nytimes.com>
2017-02-06 15:28:46 -0500

    test: remove some code duplication

--- test/live-event-test.js
+++ test/live-event-test.js
@@ -4,6 +4,14 @@ import {assert} from 'chai';
 import sinon from 'sinon';
 
 describe('LiveEvent', () => {
+  const getInstance = () => {
+    const retval = {key: 'value'};
+    const sendRequest = sinon.stub().returns(retval);
+    const le = new LiveEvent({sendRequest});
+
+    return {instance: le, retval, sendRequest};
+  };
+
   it('LiveEvent should be a Resource', () => {
     const client = {'some': 'object'};
     const le = new LiveEvent(client);
@@ -14,102 +22,82 @@ describe('LiveEvent', () => {
   });
 
   it('listInputs should list inputs of an event', () => {
-    const retval = {key: 'value'};
-    const client = {sendRequest: sinon.stub().returns(retval)};
-    const le = new LiveEvent(client);
-    const result = le.listInputs(199);
+    const testData = getInstance();
+    const result = testData.instance.listInputs(199);
 
-    assert.equal(result, retval);
-    assert(client.sendRequest.calledWith('GET', '/api/live_events/199/inputs'));
+    assert.equal(result, testData.retval);
+    assert(testData.sendRequest.calledWith('GET', '/api/live_events/199/inputs'));
   });
 
   it('eventStatus should retrieve the status of an event', () => {
-    const retval = {key: 'value'};
-    const client = {sendRequest: sinon.stub().returns(retval)};
-    const le = new LiveEvent(client);
-    const result = le.eventStatus(199);
+    const testData = getInstance();
+    const result = testData.instance.eventStatus(199);
 
-    assert.equal(result, retval);
-    assert(client.sendRequest.calledWith('GET', '/api/live_events/199/status'));
+    assert.equal(result, testData.retval);
+    assert(testData.sendRequest.calledWith('GET', '/api/live_events/199/status'));
   })
 
   it('startEvent should send request to start event', () => {
-    const retval = {key: 'value'};
-    const client = {sendRequest: sinon.stub().returns(retval)};
-    const le = new LiveEvent(client);
-    const result = le.startEvent(195);
+    const testData = getInstance();
+    const result = testData.instance.startEvent(195);
 
-    assert.equal(result, retval);
-    assert(client.sendRequest.calledWith('POST', '/api/live_events/195/start'));
+    assert.equal(result, testData.retval);
+    assert(testData.sendRequest.calledWith('POST', '/api/live_events/195/start'));
   });
 
   it('stopEvent should send request to stop event', () => {
-    const retval = {key: 'value'};
-    const client = {sendRequest: sinon.stub().returns(retval)};
-    const le = new LiveEvent(client);
-    const result = le.stopEvent(195);
+    const testData = getInstance();
+    const result = testData.instance.stopEvent(195);
 
-    assert.equal(result, retval);
-    assert(client.sendRequest.calledWith('POST', '/api/live_events/195/stop'));
+    assert.equal(result, testData.retval);
+    assert(testData.sendRequest.calledWith('POST', '/api/live_events/195/stop'));
   });
 
   it('cancelEvent should send request to cancel event', () => {
-    const retval = {key: 'value'};
-    const client = {sendRequest: sinon.stub().returns(retval)};
-    const le = new LiveEvent(client);
-    const result = le.cancelEvent(195);
+    const testData = getInstance();
+    const result = testData.instance.cancelEvent(195);
 
-    assert.equal(result, retval);
-    assert(client.sendRequest.calledWith('POST', '/api/live_events/195/cancel'));
+    assert.equal(result, testData.retval);
+    assert(testData.sendRequest.calledWith('POST', '/api/live_events/195/cancel'));
   });
 
   it('archiveEvent should send request to archive event', () => {
-    const retval = {key: 'value'};
-    const client = {sendRequest: sinon.stub().returns(retval)};
-    const le = new LiveEvent(client);
-    const result = le.archiveEvent(195);
+    const testData = getInstance();
+    const result = testData.instance.archiveEvent(195);
 
-    assert.equal(result, retval);
-    assert(client.sendRequest.calledWith('POST', '/api/live_events/195/archive'));
+    assert.equal(result, testData.retval);
+    assert(testData.sendRequest.calledWith('POST', '/api/live_events/195/archive'));
   });
 
   it('resetEvent should send request to reset event', () => {
-    const retval = {key: 'value'};
-    const client = {sendRequest: sinon.stub().returns(retval)};
-    const le = new LiveEvent(client);
-    const result = le.resetEvent(195);
+    const testData = getInstance();
+    const result = testData.instance.resetEvent(195);
 
-    assert.equal(result, retval);
-    assert(client.sendRequest.calledWith('POST', '/api/live_events/195/reset'));
+    assert.equal(result, testData.retval);
+    assert(testData.sendRequest.calledWith('POST', '/api/live_events/195/reset'));
   });
 
   it('muteEvent should send request to mute event', () => {
-    const retval = {key: 'value'};
-    const client = {sendRequest: sinon.stub().returns(retval)};
-    const le = new LiveEvent(client);
-    const result = le.muteEvent(195);
+    const testData = getInstance();
+    const result = testData.instance.muteEvent(195);
 
-    assert.equal(result, retval);
-    assert(client.sendRequest.calledWith('POST', '/api/live_events/195/mute_audio'));
+    assert.equal(result, testData.retval);
+    assert(testData.sendRequest.calledWith('POST', '/api/live_events/195/mute_audio'));
   });
 
   it('unmuteEvent should send request to unmute event', () => {
-    const retval = {key: 'value'};
-    const client = {sendRequest: sinon.stub().returns(retval)};
-    const le = new LiveEvent(client);
-    const result = le.unmuteEvent(195);
+    const testData = getInstance();
+    const result = testData.instance.unmuteEvent(195);
 
-    assert.equal(result, retval);
-    assert(client.sendRequest.calledWith('POST', '/api/live_events/195/unmute_audio'));
+    assert.equal(result, testData.retval);
+    assert(testData.sendRequest.calledWith('POST', '/api/live_events/195/unmute_audio'));
   });
 
   it('adjustAudioGain should send request to adjust audio gain with proper parameter', () => {
-    const retval = {key: 'value'};
-    const client = {sendRequest: sinon.stub().returns(retval)};
-    const le = new LiveEvent(client);
-    const result = le.adjustAudioGain(195, 3);
+    const testData = getInstance();
+    const result = testData.instance.adjustAudioGain(195, 3);
 
-    assert.equal(result, retval);
-    assert(client.sendRequest.calledWith('POST', '/api/live_events/195/adjust_audio_gain', null, {'gain': 3}));
+    assert.equal(result, testData.retval);
+    assert(testData.sendRequest.calledWith('POST', '/api/live_events/195/adjust_audio_gain', null, {'gain': 3}));
   });
 });
