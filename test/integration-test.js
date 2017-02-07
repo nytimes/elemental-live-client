36e3323e744ecf428816bee4371f9e83ab6b9bc8
Francisco Souza <francisco.souza@nytimes.com>
2017-02-07 14:31:03 -0500

    test: add infrastructure for running integration tests
    Integration tests will run only when the environment variable
    ELEMENTAL_CLIENT_INTEGRATION_TESTS_HOST is defined. This environment
    variable is used to point the tests to the proper API endpoint.
    
    The initial integration test isn't running _yet_.

---
+++ test/integration-test.js
@@ -0,0 +1,52 @@
+import ElementalClient from '../lib/elemental-client';
+
+describe('integration tests', () => {
+  const test = process.env.ELEMENTAL_CLIENT_INTEGRATION_TESTS_HOST ? it : it.skip;
+  const client = new ElementalClient(process.env.ELEMENTAL_CLIENT_INTEGRATION_TESTS_HOST);
+  let eventsToClean = [];
+
+  beforeEach(() => {
+    eventsToClean.forEach((el) => client.liveEvents().delete(el));
+    eventsToClean = [];
+  });
+
+  test('create, get, start, stop and reset a live event', () => {
+    const eventParams = {
+      'live_event': {
+        'name': `integration-tests-${Math.random()}`,
+        'input': {
+          'file_input': {
+            'uri': '/some/video/file.mov',
+          },
+        },
+        'stream_assembly': {
+          'name': 'stream1',
+          'video_description': {
+            'anti_alias': true,
+            'width': 1280,
+            'height': 720,
+            'codec': 'h.264',
+            'video_preprocessors': {
+              'deinterlace_mode': 'Adaptive',
+            },
+          },
+          'audio_description': {
+            'codec': 'aac',
+            'aac_settings': {
+              'bitrate': 64000,
+              'channels': 2,
+              'sample_rate': 44100,
+              'rate_control_mode': 'CBR',
+            },
+          },
+        },
+        'output_groups': [],
+      },
+    };
+
+    return client.liveEvents().create(eventParams).
+      then((data) => {
+        console.log(data);
+      });
+  })
+});
